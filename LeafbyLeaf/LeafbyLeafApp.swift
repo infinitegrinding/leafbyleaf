b4505c2e0a89f90f6eb49245d031508b13ad849a
infinitegrinding <matthewphraxayavong@gmail.com>
2025-07-29 17:39:15 -0700

    project created

---
+++ LeafbyLeaf/LeafbyLeafApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeafbyLeafApp.swift
+//  LeafbyLeaf
+//
+//  Created by MattHew Phraxayavong on 7/29/25.
+//
+
+import SwiftUI
+
+@main
+struct LeafbyLeafApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
